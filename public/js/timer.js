598114c2bc21b5682df69f7c3ed03099f1fb7b8f
Wasif Zaman <teatocode@gmail.com>
2017-08-23 20:47:59 -0400

    functionality complete

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,20 +1,45 @@
-const calcDiff = (t1, t2)=> {
-  // let now = new Date(),
-  //   expire = new Date(data.expire),
+const calcDiff = (t1, t2, wkds, wknds, count)=> {
   let diff = Math.floor((t2 - t1) / 1000),
-    seconds = diff % 60,
+    df = Math.floor(diff / 60 / 60 / 24) % 7;
+
+  diff = wkds ? diff : diff - count.weekdays * 24 * 60 * 60;
+  diff = wknds ? diff : diff - count.weekends * 24 * 60 * 60;
+
+  let seconds = diff % 60,
     minutes = Math.floor(diff / 60) % 60,
-    hours = Math.floor(diff / 60 / 60) % 60,
+    hours = Math.floor(diff / 60 / 60) % 24,
     days = Math.floor(diff / 60 / 60 / 24) % 7,
-    weeks = Math.floor(diff / 60 / 60 / 24 / 7);
+    weeks = Math.floor(diff / 60 / 60 / 24 / 7),
+    totaldays = Math.floor(diff / 60 / 60 / 24);
 
   return {
     seconds,
     minutes,
     hours,
     days,
-    weeks
+    weeks,
+    totaldays
   }
+}
+
+const calcDays = (t1, t2)=> {
+  let weekdays = 0,
+    weekends = 0;
+  if (t2 > t1) {
+    let ct2 = t2,
+      ct1 = t1;
+
+    ct1.setDate(ct1.getDate() + 1)
+
+    while(ct2 > ct1) {
+      let day = ct1.getDay();
+      if (day == 0 || day == 6) weekends += 1;
+      else weekdays += 1;
+      ct1.setDate(ct1.getDate() + 1);
+    }
+  }
+
+  return { weekdays, weekends }
 }
 
 const drawTimer = config=> {
@@ -25,50 +50,75 @@ const drawTimer = config=> {
       - hours, minutes, seconds
   */
   let template = `
-    <div></div>
+    <div>
+      <div>Timer to: ${config.to}</div>
+      <section></section>
+    </div>
   `;
 
   let $html = $(template);
-  $(config.parent).append($html);
+
+  if (!config.weekends) {
+    $html.append(`
+      <div>Without weekends</div>
+    `);
+  }
+
+  if (!config.weekdays) {
+    $html.append(`
+      <div class='weekdays'>Without weekdays</div>
+    `);
+  }
+
+  let $section = $html.find('section');
 
   const updateTime = ()=> {
     setTimeout(function() {
       let now = new Date();
-      let diff = calcDiff(now, config.to);
-      $html.empty();
-      drawCountDown({ parent: $html, value: diff.seconds });
-      drawCountDown({ parent: $html, value: diff.minutes });
-      drawCountDown({ parent: $html, value: diff.hours });
-      drawCountDown({ parent: $html, value: diff.days });
-      drawCountDown({ parent: $html, value: diff.weeks });
+      let diff = calcDiff(now, config.to, config.weekdays, config.weekends, config.weekdayCount);
+      $section.empty();
+      drawCountDown( $section, 'seconds', diff.seconds);
+      drawCountDown($section, 'minutes', diff.minutes);
+      drawCountDown($section, 'hours', diff.hours);
+      drawCountDown($section, 'days', diff.days);
+      drawCountDown($section, 'weeks', diff.weeks);
+      if (!config.weekends || !config.weekdays) drawCountDown($section, 'total days', diff.totaldays);
       updateTime();
     }, 1000);
   }
 
   updateTime();
+  $(config.parent).append($html);
 }
 
-const drawCountDown = config=> {
+const drawCountDown = (parent, label, value)=> {
   /*
     parent
     value
   */
   let template = `
-    <span>${config.value}</span>
+    <div>
+      <span>${label}</span>
+      <span>${value}</span>
+    </div>
   `;
 
   let $html = $(template);
-  $(config.parent).append($html);
+  $(parent).append($html);
 }
 
 
 // making a second call with ajax D:
 $.getJSON(`/json/${$('timer').data().url}`, data=> {
   // draw timer
+  let now = new Date(),
+    weekdayCount = calcDays(now, new Date(data.expire));
+
   drawTimer({
     parent: '#timer',
     to: new Date(data.expire),
     weekdays: data.weekdays,
-    weekends: data.weekends
+    weekends: false,
+    weekdayCount
   })
 })
